fa45fff68f62d74ed10fb17ada1cc019a8b5291e
mtanzim <mtanzim@gmail.com>
2019-09-07 18:10:26 +0200

    fix: data script

---
+++ data/makeWeightData.js
@@ -0,0 +1,75 @@
+var fs = require("fs");
+var inputData = JSON.parse(fs.readFileSync("inputWeights.json", "utf8"));
+
+const makeRand = ([min, max]) => Math.floor(Math.random() * (max - min)) + min;
+
+function makeData(inputData, profile) {
+  const newData = inputData.map((item, idx) => {
+    if (idx < profile.startEndIdx) {
+      return {
+        ...item,
+        waste_kg: makeRand(profile.startRange),
+        business_id: profile.id,
+        id: idx,
+        timestamp: new Date(item.timestamp)
+      };
+    } else if (idx > profile.startEndIdx && idx < profile.midEndIdx) {
+      return {
+        ...item,
+        waste_kg: makeRand(profile.midRange),
+        business_id: profile.id,
+        id: idx,
+        timestamp: new Date(item.timestamp)
+      };
+    } else {
+      return {
+        ...item,
+        waste_kg: makeRand(profile.endRange),
+        business_id: profile.id,
+        id: idx,
+        timestamp: new Date(item.timestamp)
+      };
+    }
+  });
+  return newData;
+}
+
+const profiles = [
+  {
+    id: 0,
+    title: "good",
+    startEndIdx: 20,
+    midEndIdx: 67,
+    startRange: [40, 50],
+    midRange: [30, 50],
+    endRange: [10, 20]
+  },
+  {
+    id: 1,
+    title: "med",
+    startEndIdx: 20,
+    midEndIdx: 67,
+    startRange: [40, 50],
+    midRange: [40, 50],
+    endRange: [40, 50]
+  },
+  {
+    id: 2,
+    title: "bad",
+    startEndIdx: 20,
+    midEndIdx: 67,
+    startRange: [40, 50],
+    midRange: [60, 80],
+    endRange: [85, 105]
+  }
+];
+
+profiles.forEach(profile => {
+  console.log(`Making ${profile.title}`);
+  fs.writeFile(
+    `${profile.title}.json`,
+    JSON.stringify(makeData(inputData, profile), null, 4),
+    "utf8",
+    () => console.log("Done")
+  );
+});
--- rand_weight.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var data = JSON.parse(fs.readFileSync('inputWeights.json', 'utf8'));
-
-const makeRand = (min,max) => Math.floor(Math.random() * (max - min)) + min;
-const makeData = (id,min,max) => data.map(a => ({...a, "waste_kg":makeRand(min,max), "id":id }))
-// newData.forEach(a => console.log(a));
-
-fs.writeFile('weights.json', JSON.stringify(makeData(0,1000,600)), 'utf8', () => console.log("Done"));
