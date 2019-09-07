49e6ef5fbe8d803d25cd12df6328f9515b66039a
mtanzim <mtanzim@gmail.com>
2019-09-07 20:17:51 +0200

    finish dashboard skeleton

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,64 +1,51 @@
 import React from "react";
+import { Card } from "./card";
 import Plot from "react-plotly.js";
-
+import CONSTANTS from "../../constants";
+import { defaultState } from "./config";
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
-    this.width = 800;
-    this.height = 600;
-    this.state = {
-      period: "Weekly",
-      data: [
-        { type: "bar", x: ["Jan", "Feb", "Mar"], y: [0.34, 0.55, 0.66] }
-      ],
-      layout: {
-        width: this.width,
-        height: this.height,
-        title: this.props.restaurant.name,
-        xaxis: {
-          title: "Mon"
-          // autorange: true,
-          // range: ['2019-02-17', '2019-12-16'],
-          // rangeselector: {buttons: [
-          //     {
-          //       count: 1,
-          //       label: '1w',
-          //       step: 'week',
-          //       stepmode: 'backward'
-          //     },
-          //     {
-          //       count: 6,
-          //       label: '6m',
-          //       step: 'month',
-          //       stepmode: 'backward'
-          //     },
-          //     {step: 'all'}
-          //   ]},
-          // rangeslider: {range: ['2019-02-17', '2019-12-16']},
-          // type: 'date'
-        },
-        yaxis: {
-          title: "Waste [kg]",
-        }
-      },
-      frames: [],
-      config: {}
-    };
+    this.state = defaultState;
+  }
+  componentDidMount() {
+    fetch(`${CONSTANTS.ENDPOINT}/wastes1`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: this.props.restaurant.name,
+          data: [
+            {
+              type: "line",
+              x: data.map(item => item.timestamp),
+              y: data.map(item => item.waste_kg)
+            }
+          ]
+        })
+      );
   }
 
   render() {
     return (
       <React.Fragment>
-        <div className="row border justify-content-between">
-          <Plot
-            data={this.state.data}
-            layout={this.state.layout}
-            frames={this.state.frames}
-            config={this.state.config}
-            onInitialized={figure => this.setState(figure)}
-            onUpdate={figure => this.setState(figure)}
-          />
+        <div className="row ml-2 mt-2 mr-2 mb-2 mx-auto">
+          <div className="col-8 ml-2 mt-2 mr-2 mb-2 mx-auto">
+            <Plot
+              data={this.state.data}
+              layout={this.state.layout}
+              frames={this.state.frames}
+              config={this.state.config}
+              onInitialized={figure => this.setState(figure)}
+              onUpdate={figure => this.setState(figure)}
+            />
+          </div>
+          <div className="col-2 ml-2 mt-2 mr-2 mb-2 mx-auto">
+            <Card title="Badge" content={"image here"} />
+          </div>
+          <div className="col-2 ml-2 mt-2 mr-2 mb-2 mx-auto">
+            <Card title="Score" content={"score here"}/>
+          </div>
         </div>
       </React.Fragment>
     );
--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@
 
 CONSTANTS.ERROR_MESSAGE = {};
 
-CONSTANTS.ENDPOINT = {};
+CONSTANTS.ENDPOINT = "https://5d738429214da600148f58b0.mockapi.io/api/v1/";
 
 export default CONSTANTS;
