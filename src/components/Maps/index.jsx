b0ec874feaadf7a0ff45e984bba3ec6509d0c480
mtanzim <mtanzim@gmail.com>
2019-09-07 23:49:49 +0200

    start maps

--- src/components/Maps/index.jsx
+++ src/components/Maps/index.jsx
@@ -7,18 +7,36 @@ function Maps(props) {
     fetch(`${CONSTANTS.ENDPOINT}//businesses`)
       .then(response => response.json())
       .then(data => setBusi(data));
-  },[]);
+  }, []);
   return (
     <React.Fragment>
-      <div className="ml-2 mt-2">
-        <h3>List</h3>
-        <ul>
-          {businesses &&
-            businesses.map((busi, idx) => (
-              // <li>{JSON.stringify(busi)}</li>
-              <li key={idx}>{busi.name}</li>
-            ))}
-        </ul>
+      <div className="ml-2 mt-2 mr-2 mb-2">
+        <div className="card col-8">
+          <div className="card-body">
+            <h3>List</h3>
+            <ul>
+              {businesses &&
+                businesses.map((busi, idx) => (
+                  // <li>{JSON.stringify(busi)}</li>
+                  <li key={idx}>{busi.name}</li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="ml-2 mt-2 mr-2 mb-2">
+        <div className="card col-8">
+          <div className="card-body">
+            <h3>Map</h3>
+            <ul>
+              {businesses &&
+                businesses.map((busi, idx) => (
+                  // <li>{JSON.stringify(busi)}</li>
+                  <li key={idx}>{busi.name}</li>
+                ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
