6a6b65754e28a7700566da2ebde9ad9ee640999a
mtanzim <mtanzim@gmail.com>
2019-09-08 12:16:45 +0200

    Initial dist subtree commit

---
+++ server/build/precache-manifest.6cc3f0d5484093d91aaae10967e11185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28252fd3606bf4a534aeec80af348118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2f495420205d5b8c56f",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "df03c90bbf0f2ff2054f",
+    "url": "/static/css/main.546ccb71.chunk.css"
+  },
+  {
+    "revision": "c2f495420205d5b8c56f",
+    "url": "/static/js/2.c3b8c567.chunk.js"
+  },
+  {
+    "revision": "df03c90bbf0f2ff2054f",
+    "url": "/static/js/main.55522134.chunk.js"
+  },
+  {
+    "revision": "6fb609882bc30150a4ef",
+    "url": "/static/js/runtime~main.4b05d0a5.js"
+  }
+]);
